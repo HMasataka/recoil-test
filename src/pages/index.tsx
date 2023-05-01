7337d0894042df5dc0fe9c2d7c4cc3b713d02de2
Masataka Hisasue <masataka.hisasue@knocknote.co.jp>
2023-05-01 14:19:05 +0900

    Feat use recoil

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
+import { useRecoilState, useRecoilValue } from "recoil";
+import { userState } from "store/user";
+
+const Home: React.FC = () => {
+  const [{ id, name, email }, setUser] = useRecoilState(userState);
+
+  console.log(id, name, email, setUser);
+
   return <div></div>;
-}
+};
+
+export default Home;
